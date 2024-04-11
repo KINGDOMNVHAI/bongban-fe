9c71a5d047ebca5af08fd2ef764bf644939e00e5
KINGDOMNVHAI <30685314+KINGDOMNVHAI@users.noreply.github.com>
2024-04-11 08:37:28 +0700

    Create Project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Dashboard from './pages/Dashboard';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact={true} element={<Dashboard />} />
+        <Route path="/dashboard" exact={true} element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
