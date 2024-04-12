2804dd52b73bea63e7e1f08fa119a7692a3f0f5d
KINGDOMNVHAI <30685314+KINGDOMNVHAI@users.noreply.github.com>
2024-04-12 13:25:12 +0700

    Design scrollbar of sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,8 +1,8 @@
 import Button from '@mui/material/Button';
-import { FaProductHunt } from "react-icons/fa";
-import { FaAngleRight, FaCartArrowDown } from "react-icons/fa6";
-import { MdDashboard } from "react-icons/md";
-import { IoCartOutline } from "react-icons/io5";
+import { FaProductHunt, FaLock, FaUserCircle } from "react-icons/fa";
+import { FaAngleRight, FaCartArrowDown, FaBell, FaGripVertical } from "react-icons/fa6";
+import { MdDashboard, MdMessage, MdViewKanban } from "react-icons/md";
+import { IoIosSettings } from "react-icons/io";
 
 const Sidebar = () => {
     return (
@@ -27,6 +27,139 @@ const Sidebar = () => {
                         <Button className='w-100'>
                         <span className='icon'><FaCartArrowDown/></span>
                         Orders
+                        <span className='arrow'><FaAngleRight/></span>
+                        </Button>
+                    </li>
+                    <li>
+                        <Button className='w-100'>
+                        <span className='icon'><MdMessage/></span>
+                        Messages
+                        <span className='arrow'><FaAngleRight/></span>
+                        </Button>
+                    </li>
+                    <li>
+                        <Button className='w-100'>
+                        <span className='icon'><FaBell/></span>
+                        Notifications
+                        <span className='arrow'><FaAngleRight/></span>
+                        </Button>
+                    </li>
+                    <li>
+                        <Button className='w-100'>
+                        <span className='icon'><IoIosSettings/></span>
+                        Settings
+                        <span className='arrow'><FaAngleRight/></span>
+                        </Button>
+                    </li>
+                    <li>
+                        <Button className='w-100'>
+                        <span className='icon'><FaLock/></span>
+                        Authentication
+                        <span className='arrow'><FaAngleRight/></span>
+                        </Button>
+                    </li>
+                    <li>
+                        <Button className='w-100'>
+                        <span className='icon'><FaUserCircle/></span>
+                        User
+                        <span className='arrow'><FaAngleRight/></span>
+                        </Button>
+                    </li>
+                    <li>
+                        <Button className='w-100'>
+                        <span className='icon'><MdViewKanban/></span>
+                        Kanban
+                        <span className='arrow'><FaAngleRight/></span>
+                        </Button>
+                    </li>
+                    <li>
+                        <Button className='w-100'>
+                        <span className='icon'><FaGripVertical /></span>
+                        Table Tennis
+                        <span className='arrow'><FaAngleRight/></span>
+                        </Button>
+                    </li>
+                    <li>
+                        <Button className='w-100'>
+                        <span className='icon'><FaCartArrowDown/></span>
+                        Orders
+                        <span className='arrow'><FaAngleRight/></span>
+                        </Button>
+                    </li>
+                    <li>
+                        <Button className='w-100'>
+                        <span className='icon'><MdMessage/></span>
+                        Messages
+                        <span className='arrow'><FaAngleRight/></span>
+                        </Button>
+                    </li>
+                    <li>
+                        <Button className='w-100'>
+                        <span className='icon'><FaBell/></span>
+                        Notifications
+                        <span className='arrow'><FaAngleRight/></span>
+                        </Button>
+                    </li>
+                    <li>
+                        <Button className='w-100'>
+                        <span className='icon'><IoIosSettings/></span>
+                        Settings
+                        <span className='arrow'><FaAngleRight/></span>
+                        </Button>
+                    </li>
+                    <li>
+                        <Button className='w-100'>
+                        <span className='icon'><FaCartArrowDown/></span>
+                        Orders
+                        <span className='arrow'><FaAngleRight/></span>
+                        </Button>
+                    </li>
+                    <li>
+                        <Button className='w-100'>
+                        <span className='icon'><MdMessage/></span>
+                        Messages
+                        <span className='arrow'><FaAngleRight/></span>
+                        </Button>
+                    </li>
+                    <li>
+                        <Button className='w-100'>
+                        <span className='icon'><FaBell/></span>
+                        Notifications
+                        <span className='arrow'><FaAngleRight/></span>
+                        </Button>
+                    </li>
+                    <li>
+                        <Button className='w-100'>
+                        <span className='icon'><IoIosSettings/></span>
+                        Settings
+                        <span className='arrow'><FaAngleRight/></span>
+                        </Button>
+                    </li>
+                    <li>
+                        <Button className='w-100'>
+                        <span className='icon'><FaCartArrowDown/></span>
+                        Orders
+                        <span className='arrow'><FaAngleRight/></span>
+                        </Button>
+                    </li>
+                    <li>
+                        <Button className='w-100'>
+                        <span className='icon'><MdMessage/></span>
+                        Messages
+                        <span className='arrow'><FaAngleRight/></span>
+                        </Button>
+                    </li>
+                    <li>
+                        <Button className='w-100'>
+                        <span className='icon'><FaBell/></span>
+                        Notifications
+                        <span className='arrow'><FaAngleRight/></span>
+                        </Button>
+                    </li>
+                    <li>
+                        <Button className='w-100'>
+                        <span className='icon'><IoIosSettings/></span>
+                        Settings
                         <span className='arrow'><FaAngleRight/></span>
                         </Button>
                     </li>
