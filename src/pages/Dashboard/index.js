bd93aad1c6cea64e8d36990e274282d052dbb9cd
KINGDOMNVHAI <30685314+KINGDOMNVHAI@users.noreply.github.com>
2024-04-15 14:20:13 +0700

    Create dashboard box

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,32 @@
 const Dashboard = () => {
-    return <div>
-        <h1>Dashboard</h1>
-    </div>
+    return <>
+        <section className="right-content w-100">
+            <div className="row dashboardBoxWrapperRow">
+                <div className="col-md-9">
+                    <div className="dashboardBoxWrapper d-flex">
+                        <div className="dashboardBox">
+                            <h1>Total Users</h1>
+                        </div>
+                        <div className="dashboardBox">
+                            <h1>Total Orders</h1>
+                        </div>
+                        <div className="dashboardBox">
+                            <h1>Total Orders</h1>
+                        </div>
+                        <div className="dashboardBox">
+                            <h1>Total Orders</h1>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-md-3">
+                    <div className="box">
+
+                    </div>
+                </div>
+            </div>
+        </section>
+    </>
 }
 
 export default Dashboard;
