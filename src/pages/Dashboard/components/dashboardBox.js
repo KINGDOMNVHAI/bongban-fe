353379e89bd19851b3b1525c5e77c612f4511198
KINGDOMNVHAI <30685314+KINGDOMNVHAI@users.noreply.github.com>
2024-04-15 16:44:52 +0700

    Change dashboard box to button

--- src/pages/Dashboard/components/dashboardBox.js
+++ src/pages/Dashboard/components/dashboardBox.js
@@ -1,7 +1,9 @@
+import { HiDotsVertical } from "react-icons/hi";
+import Button from '@mui/material/Button';
 
 const DashboardBox = (props) => {
     return (
-        <div className="dashboardBox" style={{
+        <Button className="dashboardBox" style={{
             backgroundImage:`linear-gradient(to right, ${props.color?.[0]}, ${props.color?.[1]})`
         }}>
             <div className="d-flex w-100">
@@ -21,7 +23,12 @@ const DashboardBox = (props) => {
                     }
                 </div>
             </div>
-        </div>
+
+            <div className="d-flex align-items-center w-100">
+                <h6 className="text-white mb-0 mt-0">Last Month</h6>
+                <Button className="ml-auto toggleIcon"><HiDotsVertical/></Button>
+            </div>
+        </Button>
     )
 }
 
