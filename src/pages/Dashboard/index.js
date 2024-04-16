9ee29f4e51289b40864df5d873b1c916087289fc
KINGDOMNVHAI <30685314+KINGDOMNVHAI@users.noreply.github.com>
2024-04-16 13:53:09 +0700

    Create button eye, edit, delete

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,13 +1,11 @@
 import { useState } from "react";
 import { IoMdCart } from "react-icons/io";
-import { FaUserCircle } from "react-icons/fa"
-import { MdShoppingBag } from "react-icons/md";
+import { FaUserCircle, FaEye, FaPencilAlt } from "react-icons/fa"
+import { MdShoppingBag, MdDelete } from "react-icons/md";
 import { GiStarsStack } from "react-icons/gi";
 
-// import { InputLabel } from "@mui/material/InputLabel";
-import { MenuItem, Select } from "@mui/material/";
+import { MenuItem, Select, FormControl, Button } from "@mui/material/";
 // import { FormHelperText } from "@mui/material/FormHelperText";
-// import { FormControl } from "@mui/material/FormControl";
 
 import DashboardBox from "./components/dashboardBox";
 
@@ -55,35 +53,81 @@ const Dashboard = () => {
                 <div className="row cardFilters mt-3">
                     <div className="col-md-3">
                         <h4>SHOW BY</h4>
-
+                        <FormControl size="small" className="w-100">
                         <Select
                             value={showBy}
                             onChange={(e) => setShowBy(e.target.value)}
                             displayEmpty
                             inputProps={{'aria-label': 'Without label'}}
+                            labelId="demo-select-small-label"
+                            className="w-100"
                         >
                             <MenuItem value=""><em>None</em></MenuItem>
                             <MenuItem value={10}>Ten</MenuItem>
                             <MenuItem value={20}>Twenty</MenuItem>
                             <MenuItem value={30}>Thirty</MenuItem>
                         </Select>
+                        </FormControl>
                     </div>
 
                     <div className="col-md-3">
                         <h4>CATEGORY BY</h4>
-
+                        <FormControl size="small" className="w-100">
                         <Select
                             value={showBysetCatBy}
                             onChange={(e) => setCatBy(e.target.value)}
                             displayEmpty
                             inputProps={{'aria-label': 'Without label'}}
+                            labelId="demo-select-small-label"
+                            className="w-100"
                         >
                             <MenuItem value=""><em>None</em></MenuItem>
                             <MenuItem value={10}>Ten</MenuItem>
                             <MenuItem value={20}>Twenty</MenuItem>
                             <MenuItem value={30}>Thirty</MenuItem>
                         </Select>
+                        </FormControl>
                     </div>
+                </div>
+
+                <div className="table-responsive mt-3">
+                    <table className="table table-bordered">
+                        <thead className="thead-dark">
+                            <tr>
+                                <th>UID</th>
+                                <th>PRODUCT</th>
+                                <th>CATEGORY</th>
+                                <th>BRAND</th>
+                                <th>PRICE</th>
+                                <th>STOCK</th>
+                                <th>RATING</th>
+                                <th>ORDER</th>
+                                <th>SALES</th>
+                                <th>ACTION</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <tr>
+                                <td>#1</td>
+                                <td>Tops and skirt set for Female</td>
+                                <td>Womans</td>
+                                <td>richman</td>
+                                <td>$21.00</td>
+                                <td>30</td>
+                                <td>4.9 (16)</td>
+                                <td>380</td>
+                                <td>$38k</td>
+                                <td>
+                                    <div className="actions d-flex align-items-center">
+                                        <Button className="secondary" color="secondary"><FaEye/></Button>
+                                        <Button className="success" color="success"><FaPencilAlt/></Button>
+                                        <Button className="error" color="error"><MdDelete/></Button>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
 
